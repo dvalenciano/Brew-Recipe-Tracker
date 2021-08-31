25049063227b9bedfc2647734eab4b9ddce1faf4
Derek <dvalenciano1986@gmail.com>
2021-08-31 13:57:18 -0500

    added gitignore to root

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const db = require('./db')
+const routes = require('./routes')
 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -25,6 +27,7 @@ app.get('/', (req, res) => {
 
 
 
+app.use('/api', routes)
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
