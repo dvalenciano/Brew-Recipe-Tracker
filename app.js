5a5ac899856767a3f2ddcacf7ee04a7e228b0f66
Derek <dvalenciano1986@gmail.com>
2021-08-31 11:31:23 -0500

    update app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+const db = require('./db')
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
