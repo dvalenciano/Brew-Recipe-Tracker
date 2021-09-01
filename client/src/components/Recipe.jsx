d1b34af9aa2e7f71f2ceddcbd100f754346be37f
Derek <dvalenciano1986@gmail.com>
2021-09-01 11:41:51 -0500

    updated index in src folder

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -5,7 +5,8 @@ const Recipe= (props) => {
 console.log(props)
   return (
     <div className='recipe'>
-      <h2>{props.recipe.title}</h2>
+      <h2>{props.recipe.name}</h2>
+      <h4>{props.recipe.category}</h4>
       <p>{props.recipe.content}</p>
       <Comment />
     </div>
