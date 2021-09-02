b70637addb34662cfe0ae949ecf39e76ea2308b1
Derek <dvalenciano1986@gmail.com>
2021-09-02 17:26:21 -0500

    updated app.js to add switch route for forum page

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,21 @@ import { Route, Switch } from 'react-router-dom'
 import Category from './components/Category'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Forum from './pages/Forum'
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <main>
-       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/recipes/:category' component={Category}/>
-        
-       </Switch>
-     </main>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/recipes/:category" component={Category} />
+          <Route path="/comments" component={Forum} />
+        </Switch>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
