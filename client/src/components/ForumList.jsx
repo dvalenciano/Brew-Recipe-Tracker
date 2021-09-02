7d320e77c34f7c892122def729c91bb531a59ec0
Derek <dvalenciano1986@gmail.com>
2021-09-02 17:20:25 -0500

    created forum component for front end comments

---
+++ client/src/components/ForumList.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const ForumList = () => {
+  const [comments, setcomments] = useState([])
+  useEffect(() => {
+    getcomments()
+  }, [])
+
+  async function getComments() {
+    const res = await axios.get(`${BASE_URL}/comments`)
+    setcomments(res.data)
+  }
+  console.log(comments)
+
+  return (
+    <div className="commentList">
+      <h2>Beer Thoughts</h2>
+
+      {comments.map((comment) => {
+        return (
+          <div className="commentItem" key={comment._id}>
+            <p>{comment.content}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ForumList
---
+++ client/src/pages/Forum.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import RecipePost from '../components/RecipePost'
+import RecipeList from '../components/RecipeList'
+
+export default function Home() {
+  const [category, setCategory] = useState('IPA')
+
+  function chooseCategory(e) {
+    setCategory(e.target.name)
+  }
+
+  return (
+    <div>
+      <button onClick={chooseCategory} name="IPA">
+        IPA
+      </button>
+      <button onClick={chooseCategory} name="Pale Ale">
+        Pale Ale
+      </button>
+      <button onClick={chooseCategory} name="Stout">
+        Stout
+      </button>
+      <button onClick={chooseCategory} name="Other">
+        Other
+      </button>
+      <RecipePost category={category} />
+      <RecipeList category={category} />
+    </div>
+  )
+}
