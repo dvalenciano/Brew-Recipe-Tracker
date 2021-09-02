7192c2e44ea4b70daa7961ade78e02afc06ed898
Derek <dvalenciano1986@gmail.com>
2021-09-02 13:59:24 -0500

    created api call to RecipeList to display recipes based on category selected

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 const RecipeList = ({ category }) => {
-  console.log(category)
+  const [recipes, setRecipes] = useState([])
+  useEffect(() => {
+    getRecipesByCategory()
+  }, [category])
+
+  async function getRecipesByCategory() {
+    const res = await axios.get(`${BASE_URL}/recipes/categories/${category}`)
+    setRecipes(res.data)
+  }
+  console.log(recipes)
+
   return (
     <div className="recipelist">
       <h2>{category} Recipes</h2>
+
+      {recipes.map((recipe) => {
+        return (
+          <div className="recipeItem" key={recipe._id}>
+            <h4>{recipe.name}</h4>
+            <p>{recipe.content}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { BASE_URL } from '../globals'
+
 import RecipeList from '../components/RecipeList'
 
 export default function Home() {
   const [category, setCategory] = useState('IPA')
 
-  // useEffect (() => {
-  //   async function getCategories() {
-  //     const res = await axios.get(`${BASE_URL}/recipes`)
-  //     console.log(res)
-  //     setCategories(res.data.results)
-  //   }
-  //   getCategories()
-  // },[])
+  function chooseCategory(e) {
+    setCategory(e.target.name)
+  }
 
   return (
     <div>
-      <button>IPA</button>
-      <button>Pale Ale</button>
-      <button>Stout</button>
-      <button>Other</button>
-
+      <button onClick={chooseCategory} name="IPA">
+        IPA
+      </button>
+      <button onClick={chooseCategory} name="Pale Ale">
+        Pale Ale
+      </button>
+      <button onClick={chooseCategory} name="Stout">
+        Stout
+      </button>
+      <button onClick={chooseCategory} name="Other">
+        Other
+      </button>
+      {/* <RecipePost category={category} /> */}
       <RecipeList category={category} />
     </div>
   )
