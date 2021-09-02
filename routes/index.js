dc69509aee74cfbd3cdbace4b959b2d0589193f3
Derek <dvalenciano1986@gmail.com>
2021-09-02 09:40:23 -0500

    Removed images model and controller

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,17 @@ router.get('/', (req, res) => res.send('This is root!'))
 
 router.post('/recipes', controllers.createRecipe)
 
-router.post('/images', controllers.createImage)
-
 router.post('/comments', controllers.createComment)
 
 router.get('/recipes', controllers.getRecipe)
-
-router.get('/images', controllers.getImage)
 
 router.get('/comments', controllers.getComment)
 
 router.get('/comments/:id', controllers.getCommentById)
 
-router.get('/images/:id', controllers.getImageById)
-
 router.get('/recipes/:id', controllers.getRecipeById)
 
 router.delete('/comments/:id', controllers.deleteComment)
-
-router.delete('/images/:id', controllers.deleteImage)
 
 router.delete('/recipes/:id', controllers.deleteRecipe)
 
